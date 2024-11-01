1abeeb4e37d62713ed832e81b9b7382d0db73ba7
Azim <azimuddin807@gmail.com>
2024-11-02 00:02:10 +0600

    react iocn

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -2,7 +2,7 @@
 
 const Book = ({book}) => {
 
-    const {image,bookName,author} = book;
+    const {image,bookName,author,tags,category,rating} = book;
 
 
     return (
@@ -12,12 +12,20 @@ const Book = ({book}) => {
             src={image}
             alt={bookName} />
         </figure>
+
+        <div className="mt-3 flex gap-3">
+            {
+                tags.map(tag => <button className="btn btn-xs bg-gray-100 text-green-400  font-bold">{tag}</button>)
+            }
+        </div>
+
         <div className="card-body">
           <h2 className="card-title"> {bookName} </h2>
           <p>By : {author}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <div className="border-t-2 border-dashed border-gray-300 py-1"></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            <div className="badge ">{rating}</div>
           </div>
         </div>
       </div>
